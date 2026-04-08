a169d82de124a1807af5e6d98fed918148c62dc9
merajshaikh3 <47921927+merajshaikh3@users.noreply.github.com>
2026-04-08 10:29:14 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# nodejs-typescript-template
+Template for a Node.js + Typescript backend project
---
+++ src/server.ts
@@ -0,0 +1,2 @@
+console.log("This is a nodejs-typescript template");
+console.log("I'm adding a new line to this code");
